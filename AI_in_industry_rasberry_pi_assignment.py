6000013127d12a357239026dac0492c5e08816d1
strangeman09 <87604490+strangeman09@users.noreply.github.com>
2023-09-27 22:34:04 +0530

    Update AI_in_industry_rasberry_pi_assignment.py

--- AI_in_industry_rasberry_pi_assignment.py
+++ AI_in_industry_rasberry_pi_assignment.py
@@ -2,27 +2,27 @@ import RPi.GPIO as GPIO
 import time
 from firebase import firebase
 
-# Initialize Firebase
+
 firebase = firebase.FirebaseApplication('YOUR_FIREBASE_URL', None)
 
-# Define GPIO pin for IR sensor
+
 ir_sensor_pin = 17  # Example pin, change it according to your wiring
 
-# Set up GPIO
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(ir_sensor_pin, GPIO.IN)
 
-# Function to read IR sensor data
+
 def read_ir_sensor():
     return GPIO.input(ir_sensor_pin)
 
-# Main loop
+
 while True:
     ir_value = read_ir_sensor()
 
-    # Send data to Firebase
+    
     data = {'ir_value': ir_value}
     firebase.post('/sensor_data', data)
 
-    # Add a delay to control the data sending rate
-    time.sleep(1)
+    
+    time.sleep(5)
